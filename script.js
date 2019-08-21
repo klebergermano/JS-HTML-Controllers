b856cff78a2d755b907e47d020bfe99db0df2c3e
Kleberus <kleber_lsgermano@hotmail.com>
2019-08-21 10:16:30 -0300

    count finish

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-alert('work!');
+
+var start_number = document.querySelector('#start_number');
+var end_number = document.querySelector('#end_number');
+var int_number = document.querySelector('#int_number');
+
+var res = document.querySelector('#res');
+
+
+
+function countNumber() {
+
+    var start = Number(start_number.value);
+    var end = Number(end_number.value);
+    var interval = Number(int_number.value);
+
+    res.innerHTML = '';
+    var c = 0;
+    for (var i = start; i <= end; i++) {     
+       
+
+           if (c == interval || i == start) {
+               res.innerHTML += '<p>' + i + '</p>';
+               c = 0;
+           } else {
+               c++;         
+           }
+         
+
+    }
+
+
+
+
+}
+
+
+
