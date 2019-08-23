43bc85fee5a8b977ab92dfd3b53e2593381e391c
Kleberus <kleber_lsgermano@hotmail.com>
2019-08-23 07:10:47 -0300

    Number Analyzer add

--- script.js
+++ script.js
@@ -1,19 +1,140 @@
 
-var start_number = document.querySelector('#start_number');
-var end_number = document.querySelector('#end_number');
-var int_number = document.querySelector('#int_number');
-var res = document.querySelector('#res');
+
+/*-------------------------------------- Number Analyzer ---------------*/
+
+var input_number = document.querySelector('#input_number');
+var  select = document.querySelector('#select_numbers');
+
+var add_number = document.querySelector('#btn_add_number');
+var analyze = document.querySelector('#btn_finish_analyze');
+var clear = document.querySelector('#btn_clear');
+
+
+var result = document.querySelector('#result');
+
+add_number.addEventListener('click', setNumber);
+analyze.addEventListener('click', analyzeNumber);
+clear.addEventListener('click', clearSelect);
 
 
 
+function clearSelect() {
+    select.innerHTML = '';
+    result.innerHTML = '';
+
+
+}
+
+function analyzeNumber() {
+    let averageNum = 0;
+    let biggestNum = 0;
+    let smallestNum = 0;
+    let sumNum = 0;
+    let optionsNum = select.childElementCount;
+    let arrayNum= [];
+
+
+    if (optionsNum < 2) {
+        alert('Please insert 2 or more value');
+    } else {
+
+
+        for (let i = 0; i < optionsNum; i++) {
+
+            let num = Number(select.options[i].text)
+            sumNum += num;
+            arrayNum.push(num);
+
+        }
+        arrayNum.sort(function (a, b) { return a - b });
+        smallestNum = arrayNum[0];
+        biggestNum = arrayNum[(optionsNum - 1)];
+        averageNum = sumNum / optionsNum;
+        result.innerHTML = '';
+        resultTxt = `The smallest number is ${smallestNum}<br/>`;
+        resultTxt += `The biggest number is ${biggestNum} <br/>`;
+        resultTxt += `The average number is ${averageNum} <br/>`;
+        resultTxt += `The sume of the numbers is ${sumNum} <br/>`;
+        result.innerHTML = resultTxt;
+
+
+    }
+
+
+}
+
+
+
+
+function setNumber() {
+    let number = input_number.value;
+    let valida = validaNumber(number);
+    if (valida) { addNumber(number);  } 
+}
+
+
+
+function findNumber(n1) {
+   let c = select.childElementCount;
+    let finded = false; 
+    for (let i = 0; i < c; i++) {
+     
+        let num = select.options[i].text;
+ 
+        if (n1 == num) {  finded = true;  }
+    }
+
+    if (finded) {
+       
+        return true;
+    }
+
+}
+
+function addNumber(number) {
+    let option = document.createElement('option');
+    option.text = number;
+    select.appendChild(option);
+    input_number.value = '';
+}
+
+function validaNumber(num) {
+
+    let finded = findNumber(num);
+
+    if (num > 100 || num == '') {
+
+        alert('Please enter with a number greater than 0 and less than 100');
+        return false;
+
+    } else if (finded) {
+
+        alert('This number already exist, please chose another one');
+        return false;
+
+    }
+
+    else {
+        return true;
+    }
+
+
+}
+/*--------------------------------------Count Number---------------*/
 function countNumber() {
 
+    let start_number = document.querySelector('#start_number');
+    let end_number = document.querySelector('#end_number');
+    let int_number = document.querySelector('#int_number');
+    let res = document.querySelector('#res');
 
+    let start = Number(start_number.value);
+    let end = Number(end_number.value);
+    let interval = Number(int_number.value);
 
-    var start = Number(start_number.value);
-    var end = Number(end_number.value);
-    var interval = Number(int_number.value);
-
+    if (interval == 0) {
+        interval = 1;
+    }
     if (end == 0) {
         alert('Please digit a end number greater than zero');
 
@@ -21,7 +142,7 @@ function countNumber() {
 
         res.innerHTML = '';
 
-        for (var i = start; i >= end; i -= interval) {
+        for (let i = start; i >= end; i -= interval) {
             res.innerHTML += '<p>' + i + '</p>';
         }
 
@@ -29,7 +150,7 @@ function countNumber() {
 
         res.innerHTML = '';
 
-        for (var i = start; i <= end; i+=interval) {
+        for (let i = start; i <= end; i+=interval) {
                 res.innerHTML += '<p>' + i + '</p>';
                 }
     }
@@ -41,21 +162,28 @@ function countNumber() {
 
 }
 
-
+/*--------------------------------------Multiply---------------*/ 
 function multiply() {
-    var number = document.querySelector('#multiply_number').value;
-    var txt_table = document.querySelector('#txt_table');
+    let number = document.querySelector('#multiply_number').value;
+    let txt_table = document.querySelector('#txt_table');
 
     
     if (number > 0) {
         txt_table.innerHTML = '';
-        for (var i = 0; i <= 10; i++) {
+        for (let i = 0; i <= 10; i++) {
+            txt_table.size = '12';
 
-            txt_table.innerHTML += '<p>'+ i +'x'+number+'='+(i*number)+'</p>';
-          
+            let option = document.createElement('option');
+
+
+            option.innerText = `${i} x ${number} = ${i * number}`;
+            txt_table.appendChild(option);
+
         }
 
-    } 
+    } else { alert('Please insert a number greater than 0');}
 
 }
+
+
 
